f23aa63452529490b421b8052e8da67c4c410a56
Admin 2 <admin2@Dingus-MacBook-Pro.local>
2019-08-18 20:39:17 +0530

    zsh setup

---
+++ setup_commands.sh
@@ -0,0 +1,8 @@
+
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
